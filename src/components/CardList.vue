ad5547d12adfc1c375f8039b58498eab27190b9c
IrynaVelykholova <irynavely93@gmail.com>
2023-08-31 01:53:01 +0200

    hover aggiunto

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -33,18 +33,21 @@ export default {
                     <!-- immagine film -->
                     <img :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`">
                     <!-- testo film-->
-                    <div>
-                        <div class="text-center"><strong>Titolo Film: </strong>{{ movie.title }}</div>
-                        <div class="text-center"><strong>Titolo originale: </strong> {{ movie.original_title }} </div>
-                        <div class="text-center"><strong>Lingua: </strong><lang-flag :iso="movie.original_language"/></div>
-                        <!-- stelle -->
-                        <div class="text-center">
-                            <span><strong>Voto: </strong></span>
-                            <div class="stars-outer">
-                                <div class="stars-inner" :style="`width: ${votingStars(movie.vote_average)}`"></div>
+                    <div class="hover-text d-flex justify-content-center align-items-center">
+                        <div>
+                            <div class="text-center text-primary"><strong>Titolo Film: </strong>{{ movie.title }}</div>
+                            <div class="text-center text-primary"><strong>Titolo originale: </strong> {{ movie.original_title }} </div>
+                            <div class="text-center text-primary"><strong>Lingua: </strong><lang-flag :iso="movie.original_language"/></div>
+                            <!-- stelle -->
+                            <div class="text-center text-warning">
+                                <span><strong>Voto: </strong></span>
+                                <div class="stars-outer">
+                                    <div class="stars-inner" :style="`width: ${votingStars(movie.vote_average)}`"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    
                 </div>
                 
             </div>
@@ -59,18 +62,21 @@ export default {
                     <!-- immagine film -->
                     <img :src="`http://image.tmdb.org/t/p/w500/${serie.poster_path}`">
                     <!-- testo film-->
-                    <div>
-                        <div class="text-center"><strong>Titolo Serie TV: </strong>{{ serie.title }}</div>
-                        <div class="text-center"><strong>Titolo originale: </strong> {{ serie.original_title }} </div>
-                        <div class="text-center"><strong>Lingua: </strong><lang-flag :iso="serie.original_language"/></div>
-                        <!-- stelle -->
-                        <div class="text-center">
-                            <span><strong>Voto: </strong></span>
-                            <div class="stars-outer">
-                                <div class="stars-inner" :style="`width: ${votingStars(serie.vote_average)}`"></div>
+                    <div class="hover-text d-flex justify-content-center align-items-center">
+                        <div>
+                            <div class="text-center text-primary"><strong>Titolo Serie TV: </strong>{{ serie.title }}</div>
+                            <div class="text-center text-primary"><strong>Titolo originale: </strong> {{ serie.original_title }} </div>
+                            <div class="text-center text-primary"><strong>Lingua: </strong><lang-flag :iso="serie.original_language"/></div>
+                            <!-- stelle -->
+                            <div class="text-center text-warning">
+                                <span><strong>Voto: </strong></span>
+                                <div class="stars-outer">
+                                    <div class="stars-inner" :style="`width: ${votingStars(serie.vote_average)}`"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    
                 </div>
             </div>
         </div>
@@ -79,11 +85,28 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-    height: 450px;
-}
-img {
     height: 300px;
+    img {
+        height: 100%;
     }
+    .hover-text {
+        background-color: rgba(0, 0, 0, 0.650);
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        opacity: 0;
+        transition: opacity 0.5s;
+        backdrop-filter: blur(1rem);
+    }
+    &:hover {
+        .hover-text {
+            opacity: 1;
+        }
+    }
+}
+//stelle
 .stars-outer {
     display: inline-block;
     position: relative;
@@ -107,5 +130,4 @@ img {
     content: "\f005 \f005 \f005 \f005 \f005";
     color: #f8ce0b;
 }
-
 </style>
